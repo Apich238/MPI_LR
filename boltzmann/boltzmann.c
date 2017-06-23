09fae5ee3875e0b3a757bc0652cf048120a935e9
Andrew Yefanov <1134togo@gmail.com>
2017-06-23 10:47:07 +0300

    Переименованы поля структур.

--- boltzmann/boltzmann.c
+++ boltzmann/boltzmann.c
@@ -1,76 +1,77 @@
 #include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <malloc.h>
 
 int rank, size;
 
-double w[] = { 4.0 / 9,
-1.0 / 9 ,1.0 / 9 ,1.0 / 9 ,1.0 / 9 ,
-1.0 / 36 ,1.0 / 36 ,1.0 / 36 ,1.0 / 36 };
+double weights[] = {4.0 / 9,
+                    1.0 / 9, 1.0 / 9, 1.0 / 9, 1.0 / 9,
+                    1.0 / 36, 1.0 / 36, 1.0 / 36, 1.0 / 36};
 
 typedef struct GridNode {
-	double MDen;		//макроскопическая плотность
-	double MVel[2];		//макроскопическая скорость, 0 - горизонтельно, 1 - вертикально
-	double feq[9];		//
-	double f[9];
+    double macroscopicDensity;        //макроскопическая плотность
+    double macroscopicVelocity[2];        //макроскопическая скорость, 0 - горизонтельно, 1 - вертикально
+    double equilibriumDistribution[9];        //
+    double particleDistribution[9];
 } GridNode;
 
 typedef struct Grid {
-	int H, W;			//размеры сетки
-	double tau,c;		//время релаксации и скорость сетки
-	GridNode** nds;
+    int height, width;            //размеры сетки
+    double relaxationTime, latticeSpeed;        //время релаксации и скорость сетки
+    GridNode **nodes;
 } Grid;
 
-void InitGrid(Grid* pg) {
-	//инициализация решётки
+void InitGrid(Grid *pg, int gridSize) {
+    //инициализация решётки
+    pg->height = pg->width = gridSize;
 }
 
-void FreeGrid(Grid* pg) {
-	//высвобождение ресурсов решётки
+void FreeGrid(Grid *pg) {
+    //высвобождение ресурсов решётки
 }
 
-void Streaming(Grid* pg) {
-	//обработка распространения
-	for (int i = 0; i < pg->H; i++) {
-		for(int j=0;j<pg->W)
-	}
+void Streaming(Grid *pg) {
+    //обработка распространения
+    for (int i = 0; i < pg->height; i++) {
+        for (int j = 0; j < pg->width; j++) {
+
+        }
+    }
 }
 
-void CalcMacro(Grid* pg) {
-	//перерасчёт макроскопических плотностей, скоростей, fi eq
+void CalcMacro(Grid *pg) {
+    //перерасчёт макроскопических плотностей, скоростей, fi eq
 
 }
 
-void Collide(Grid* pg) {
-	//обработка столкновений
+void Collide(Grid *pg) {
+    //обработка столкновений
 }
 
-void Snapshot(Grid* pg) {
-	//сохранение снимка
+void Snapshot(Grid *pg) {
+    //сохранение снимка
 }
 
 void SaveSnapshots() {
-	//сохранение снимков
+    //сохранение снимков
 }
 
-int main(int argc, char* argv[]) {
-	MPI_Init(&argc, &argv);
-	MPI_Comm_size(MPI_COMM_WORLD, &size);
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-	Grid g;
-	int size = 10;
-	InitGrid(&g, 10);
-	int totaltime = 10000;
-	int snapshoprate = 1000;
-	for (int i = 0; i < totaltime; i++) {
-		Streaming(&g);
-		CalcMacro(&g);
-		Collide(&g);
-		if (i%snapshoprate == 0)
-			Snapshot(&g);
-	}
-	SaveSnapshots();
-	FreeGrid(&g);
-	MPI_Finalize();
+int main(int argc, char *argv[]) {
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    Grid grid;
+    int size = 10;
+    InitGrid(&grid, 10);
+    int totaltime = 10000;
+    int snapshoprate = 1000;
+    for (int i = 0; i < totaltime; i++) {
+        Streaming(&grid);
+        CalcMacro(&grid);
+        Collide(&grid);
+        if (i % snapshoprate == 0) {
+            Snapshot(&grid);
+        }
+    }
+    SaveSnapshots();
+    FreeGrid(&grid);
+    MPI_Finalize();
 }
